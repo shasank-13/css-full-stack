5f9c882ade331401b03426b2c9e6334a2ebd4683
shasank-13 <shasankthupakula@gmail.com>
2025-12-20 09:23:43 +0530

    Update image sources and content in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showHome() {
     document.getElementById("content").innerHTML = `
         <h2>Home</h2>
-        <img src="images/home.jpg" class="section-img" alt="Home Image">
+        <img src="project image.jpg" class="section-img">
         <p>Welcome to our website. This is the home section.</p>
     `;
 }
@@ -9,7 +9,7 @@ function showHome() {
 function showAbout() {
     document.getElementById("content").innerHTML = `
         <h2>About Us</h2>
-        <img src="images/about.jpg" class="section-img" alt="About Image">
+        <img src="projectH.jpg" class="section-img">
         <p>We are a team learning web development using HTML, CSS, and JavaScript.</p>
     `;
 }
@@ -17,7 +17,7 @@ function showAbout() {
 function showServices() {
     document.getElementById("content").innerHTML = `
         <h2>Our Services</h2>
-        <img src="images/services.jpg" class="section-img" alt="Services Image">
+        <img src="projectH1.jpg" class="section-img">
         <ul>
             <li>Web Design</li>
             <li>Frontend Development</li>
@@ -29,8 +29,8 @@ function showServices() {
 function showContact() {
     document.getElementById("content").innerHTML = `
         <h2>Contact Us</h2>
-        <img src="images/contact.jpg" class="section-img" alt="Contact Image">
+        <img src="projectH1.jpg" class="section-img">
         <p>Email: example@email.com</p>
-        <p>Phone: +91 9000000005</p>
+        <p>Phone: +91 9*********5</p>
     `;
 }
