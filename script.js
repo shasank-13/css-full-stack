57a498fd3806894b46ee4f4c5f52c6c1fb73a131
shasank-13 <shasankthupakula@gmail.com>
2025-12-20 09:26:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function showHome() {
+// HTML program1
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset= "UFT-8">
+    <title>Simple Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- <!HEADER --> 
+     <header>
+        <h1>My Simple Websiye</h1>
+        <nav>
+            <u1>
+                <li><a href="#" onclick="showHome()">Home</a></li>
+                <li><a href="#" onclick="showAbout()">About</a></li>
+                <li><a href="#" onclick="showServices()">Services</a></li>
+                <li><a href="#" onclick="showContact()">Contact</a></li>
+            </u1>
+        </nav>
+    </header>
+
+    <!-- MAIN CONTENT -->
+    <main>
+        <section id="content">
+            <h2>Welcome!</h2>
+            <p>This is the Home section. Click the menu to explore.</p>
+        </section>
+    </main>
+    <!-- FOOTER -->
+    <footer>
+        <p>@ 2025 My Simple Website</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
+*/
+
+// java script program2
+/* function showHome() {
     document.getElementById("content").innerHTML = `
         <h2>Home</h2>
         <img src="project image.jpg" class="section-img">
@@ -34,3 +75,59 @@ function showContact() {
         <p>Phone: +91 9*********5</p>
     `;
 }
+*/
+// style.css
+/* 
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+.section-img {
+    width: 100%;
+    max-width: 400px;
+    display: block;
+    margin: 15px auto;
+    border-radius: 8px;
+}
+header {
+    background-color: black;
+    color: white;
+    padding: 15px;
+
+}
+header h1 {
+    margin: 0;
+
+}
+nav u1 {
+    list-style: none;
+    padding: 0;
+    margin-top: 10px;
+    display: flex;
+}
+nav ul li a:hover {
+    color: white;
+    text-decoration: solid;
+    font-weight: bold;
+}
+nav ul li a:hover {
+    text-decoration: underline;
+}
+main{
+    padding: 20px;
+    min-height: 300px;
+}
+#content {
+    background-color: #f4f4f4;
+    padding: 20px;
+    border-radius: 5px;
+}
+footer {
+    background-color: #333;
+    color: white;
+    text-align: center;
+    padding: 10px;
+}
+*/
+
